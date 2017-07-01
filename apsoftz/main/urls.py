59099c201fda7d1a1c963ffd22a08f3ff76bdce2
Ayush <patel.ayush08@gmail.com>
2017-07-01 23:43:57 +0530

    All templates ready

--- apsoftz/main/urls.py
+++ apsoftz/main/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import  url
 from . import views
 
 urlpatterns =[
-    url('^/$',views.index,name='index'),
+    url('^$',views.index,name='index'),
+    url('^about/$',views.about,name='about'),
+    url('^contact/$',views.contact,name='about'),
+    url('^social/$',views.social,name='social'),
+    url('^git/$',views.git,name='git'),
 ]
--- apsoftz/main/views.py
+++ apsoftz/main/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request,'main/index.html',{})
+
+def about(request):
+    return render(request,'main/about.html',{})
+
+def contact(request):
+    return render(request,'main/contact.html',{})
+
+def social(request):
+    return render(request,'main/social.html',{})
+
+def git(request):
+    return render(request,'main/git.html',{})
